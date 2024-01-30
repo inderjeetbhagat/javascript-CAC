a9c54fec28cf3613e35a6f752d385fd591923e36
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-01-30 21:36:42 +0530

    string done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,3 +11,21 @@ console.log(valueInNumber1);//NaN
 
 //"33" => 33
 //"33abe" => NaN
+
+console.log("1" + 2 + 2); //122
+console.log(2 + 2 + "1"); // 41
+
+// == and equality check is different(<,>,>=)
+// === strict check
+
+//////////////////////////
+// Stack (primitive) -> You get a copy , Heap (Non Primitive) -> you get a reference of the original value
+
+let nonPrimitive1 = {
+    name : "inder",
+    id : 7
+}
+    
+let nonPrimitive2 = nonPrimitive1
+nonPrimitive2.name = "Dhoni"
+console.log(nonPrimitive1);
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "inder"
+const repoCount = 50
+
+console.log(`Hello ${name}`);
+
+const gameName = new String('inder')
