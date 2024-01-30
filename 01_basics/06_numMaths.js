6cccb9288a26cf2c31d1281d1fae19ae3579cbe4
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-01-30 22:50:56 +0530

    Maths and Num Done

---
+++ 01_basics/06_numMaths.js
@@ -0,0 +1,16 @@
+const balance = new Number(10);
+/*
+slice()
+toLocalString('en-IN')
+Math.abs()
+Math.round(4.3)
+(Math.random()*10) + 1
+
+min = 10
+max = 20
+
+Math.floor(Math.random() * (max - min + 1)) + min
+*/
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
