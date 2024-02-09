7c14e8aa334841ec12d18c097966edb3487b5deb
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-02-09 10:12:47 +0530

    scope completed

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,24 @@
+// var c = 300;
+
+if(true){
+    let a = 10;
+    const b = 20;
+    // var c = 30;
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c); //30
+
+
+function one(){
+    const name = "inder"
+    function two(){
+        const website = "youtube"
+        console.log(name);
+    }
+    // console.log(website)
+    two()
+}
+one()
